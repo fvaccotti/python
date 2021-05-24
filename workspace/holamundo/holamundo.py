24b45447fc5224f3e7d2ed2cc9a00bc3dc984239
fvaccotti <67241997+fvaccotti@users.noreply.github.com>
2021-05-24 00:48:51 -0300

    first commit all files

---
+++ workspace/holamundo/holamundo.py
@@ -0,0 +1,22 @@
+print('Hola Mundo')
+
+#REPL stands for Read, Evaluate, Print, Loop. 
+#The REPL is how you interact with the Python Interpreter. 
+#Unlike running a file containing Python code, in the REPL you can type commands and instantly see the output printed out.
+#Se utilizan las operaciones directamente sobre la consola utilizando el comando python
+#Para salir del REPL podemos poner exit()
+
+#C:\Users\fvacc\Documents\Faby\Python\python>python
+#Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+#Type "help", "copyright", "credits" or "license" for more information.
+#>>> 2 + 5
+#7
+#>>> 5 - 4
+#1
+#>>> 2 * 5
+#10
+#>>> 10 / 2
+#5.0
+#>>> 10 / 3
+#3.3333333333333335
+# >>> exit()
